fcce53363a98fe4eecee08d3555863ca26a400be
Tom Yang <xy2286@columbia.edu>
2018-10-31 23:48:05 -0400

    string

---
+++ js_data_type_2.js
@@ -0,0 +1,8 @@
+//  \ back slash to escape characters
+// \unnnn nnnn is hexadecimal, to represent unicode characters
+console.log("there ia this quote \: \" be yourself \" " + '\u020a');
+
+// strings are immutable, will expand on this
+
+
+// toString() method is on object, string, number, boolean
